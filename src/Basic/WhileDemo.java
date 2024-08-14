2cf787a4830fdc629636401e70ae8e9f60f77856
jooyeoniya <jooyeonstudyios@gmail.com>
2024-08-14 12:36:17 +0900

    while문 2

--- src/Basic/WhileDemo.java
+++ src/Basic/WhileDemo.java
@@ -1,5 +1,7 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class WhileDemo {
   public static void main(String[] args) {
     // 8월 14일
@@ -20,6 +22,38 @@ public class WhileDemo {
       } else {
         System.out.println(count);
         count++;
+      }
+    }
+
+
+    Scanner in = new Scanner(System.in);
+    int money = 0;
+
+    Outter: while (true) {
+      System.out.println("1.예금 2.출금 3.잔금 4.종료");
+
+      int number = in.nextInt();
+
+      switch (number) {
+        case 1 -> {
+          System.out.println("금액");
+          int inputMoney = in.nextInt();
+          money += inputMoney;
+          System.out.println(money);
+        }
+        case 2 -> {
+          System.out.println("금액");
+          int inputMoney = in.nextInt();
+          money -= inputMoney;
+          System.out.println(money);
+        }
+        case 3 -> {
+          System.out.println(money);
+        }
+        default -> {
+          System.out.println("종료");
+          break Outter;
+        }
       }
     }
   }
