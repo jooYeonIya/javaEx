0eb1f3e445abc87b34d041853a823bccfe2256f5
jooyeoniya <jooyeonstudyios@gmail.com>
2024-08-27 11:32:39 +0900

    제네릭의 상속 및 타입 한정

--- src/advancedJava/GenericDemo2.java
+++ src/advancedJava/GenericDemo2.java
@@ -9,6 +9,11 @@ public class GenericDemo2 {
     System.out.println(beer.name);
 
     //c1.setBeverage(new Boricha());
+
+    CupInbeverage<Beer> cupBeer = new CupInbeverage<>();
+
+    // 이건 안된다. 왜냐면 베버리지를 상속받은 타입이 아니니까
+//    CupInbeverage<String> cupStr = new CupInbeverage<String>();
   }
 }
 
@@ -22,4 +27,12 @@ class Cup<A> {
   public void setBeverage(A beverage) {
     this.beverage = beverage;
   }
+}
+
+// beverage를 상속받은 타입만 사용할 수 있도록 해라
+class CupInbeverage< T extends Beverage>{
+}
+
+// 인터페이스지만 extends를 써서 표현
+class CupExtendsInterface<T extends Comparable>{
 }
