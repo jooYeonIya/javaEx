bcb2474eacfc02fc896face2853779b1c09d5a60
jooyeoniya <jooyeonstudyios@gmail.com>
2024-08-29 11:38:57 +0900

    조급한 연산과 느긋한 연산

---
+++ src/advancedJava2/LazyDemo.java
@@ -0,0 +1,20 @@
+package advancedJava2;
+
+import java.util.stream.IntStream;
+
+public class LazyDemo {
+  public static void main(String[] args) {
+    IntStream is = IntStream.rangeClosed(1, 5);
+
+    // IntStream을 반환
+    is.filter(i -> {
+      System.out.println("filter" + i);
+      return i % 2 == 0;
+    }).map(i -> {
+      System.out.println("map" + i);
+      return i * i;
+    });
+
+
+  }
+}
