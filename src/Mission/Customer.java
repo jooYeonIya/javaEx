b04c987a660c40816103b0252bb511f3c6726495
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-03 09:38:19 +0900

    고객 메서드 내용 추가

--- src/Mission/Customer.java
+++ src/Mission/Customer.java
@@ -1,8 +1,13 @@
 package Mission;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Customer {
+  // 고객: 고객 번호 + 고객 객체
+  public Map<String, Customer> customerMap = new HashMap<>();
+
   BankCode bankCode;      		// 은행코드
   String customerNumber; 			// 고객번호 주민번호 앞자리 6자리
   String name;           			// 고객 이름
@@ -16,6 +21,24 @@ public class Customer {
     this.firstTransactionDate = LocalDate.now();
     this.lastTransactionDate = LocalDate.now();
   }
+
+  //고객의 cru 는 거래가 있을때 같이 진행됨, 고객정보 삭제는 이번에는 고려대상이 아님
+
+  // 고객 조회
+  public Customer customerInquiry(String customerNumber) {
+      Customer customer = customerMap.get(customerNumber);
+
+    if (customer == null) {
+      System.out.println("고객 정보가 없습니다");
+    }
+
+    return customer;
+  }
+
+  // 고객 거래 발생 시 업데이트
+  public void setLastTransactionDate(String customerNumber) {
+    Customer customer = customerInquiry(customerNumber);
+    customer.lastTransactionDate = LocalDate.now();
+  }
 }
 
-//고객의 cru 는 거래가 있을때 같이 진행됨, 고객정보 삭제는 이번에는 고려대상이 아님
