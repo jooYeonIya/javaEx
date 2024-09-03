0718e205a43b99c9a4a1c8823314126cafdf6395
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-03 10:17:13 +0900

    입출금내역 클래스에 고객 클래스 추가

--- src/Mission/Transaction.java
+++ src/Mission/Transaction.java
@@ -1,16 +1,24 @@
 package Mission;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Transaction {
-  long seqNo;        		// 거래 고유 번호
-  TransactionType type; 	// 거래 타입 구분 코드
-  LocalDate date; 		// 거래 일시
-  String accountNumber; 	// 계좌 번호
-  String customerNumber;	// 고객 번호
-  long money; 			// 금액
-  String bankerNumber; 	// 직원 번호
+  // 은행별 입출금내역: 시퀀스 넘버 + 입출금 내역 객체
+  public Map<String, Transaction> transactionMap = new HashMap<>();
+
+  long seqNo;        		    // 거래 고유 번호
+  TransactionType type; 	  // 거래 타입 구분 코드
   TransactionStatus status; // 처리 상태
+  LocalDate date; 		      // 거래 일시0
+  long money; 			        // 금액
+  Customer customer;        // 고객
+  Banker banker; 	          // 직원 번호
+
+  public void updateTransactionStatus() {
+
+  }
 }
 
 
